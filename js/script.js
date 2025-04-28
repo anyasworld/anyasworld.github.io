e3dfcebaa427d05d8fd75466848d9924c36974c5
Anton Buzanov <abuzanov@hse.ru>
2025-04-28 18:43:50 +0300

    better storing

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function goToMap() {
+    // Open the map.html page
+    window.location.href = "map.html";
+  }
+
+function goToAviasales() {
+    // Redirect to aviasales.ru
+    window.location.href = "http://aviasales.ru";
+}
